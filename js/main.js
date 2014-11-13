8b621522473cd10030e7aff76537bf92869c7ea1
Mario Cuba <mario@mariocuba.net>
2014-11-13 18:24:02 +0000

    Rudimentary modification of tasks. Data types converted to strings.

--- js/main.js
+++ js/main.js
@@ -24,41 +24,43 @@ if (DEBUG == true) {
 	localStorage.clear();	
 
 	// default tasks
+	// NOTE: using strings in number and booleans because of all the string conversions made both
+	// in JSON and HTML .val()
 	app.tasks = [{
 		id: generateHash(),
 		title: "Pack bags",
 		note: "Don't forget the gadgets!",
 		due: "2014-11-20", // @TODO: use the datetime object
-		priority: 2,
-		done: false
+		priority: "2",
+		done: "false"
 	}, {
 		id: generateHash(),
 		title: "Review entries",
 		note: "Check Zendesk latest entries for info", // @TODO: overflowing makes the task disappear
-		due: null,
-		priority: 1,
-		done: false
+		due: "",
+		priority: "1",
+		done: "false"
 	}, {
 		id: generateHash(),
 		title: "Start investigation in Library",
-		note: null,
-		due: null,
-		priority: 3,
-		done: false
+		note: "",
+		due: "",
+		priority: "3",
+		done: "false"
 	}, {
 		id: generateHash(),
 		title: "Evaluate proposal",
-		note: null,
+		note: "",
 		due: "2014-11-18",
-		priority: 1,
-		done: false
+		priority: "1",
+		done: "false"
 	}, {
 		id: generateHash(),
 		title: "Develop system",
 		note: "Web and Mobile Technologies",
 		due: "2014-11-23",
-		priority: 1,
-		done: true
+		priority: "1",
+		done: "true"
 	}];
 
 	// store them
@@ -88,13 +90,13 @@ function renderTask(data, which) {
 	// make sure only the appropriate tasks are rendered
 	switch(which) {
 		case "incomplete":
-			if (data.done == true) {
+			if (data.done == "true") {
 				exit = true;
 			}
 		break;
 
 		case "complete":
-			if (data.done == false) {
+			if (data.done == "false") {
 				exit = true;
 			}
 		break;
@@ -114,9 +116,9 @@ function renderTask(data, which) {
 		.attr("data-task", data.id)
 		.html('<i class="fa fa-circle-o fa-2x fa-fw"></i>').appendTo(item);
 
-	if (data.note != null) {
+	if (data.note != "") {
 		title = data.title + "<br /><small>" + data.note + "</small>";
-	} else if (data.due != null) {
+	} else if (data.due != "") {
 		title = data.title + "<br /><small>" + data.due + "</small>";
 	} else {
 		title = data.title;
@@ -145,19 +147,19 @@ function addTask(data) {
 	data.done = false;
 
 	if (data.note == undefined) {
-		data.note = null;
+		data.note = "";
 	}
 
 	if (data.due == undefined) {
-		data.due = null;
+		data.due = "";
 	}
 
 	if (data.priority == undefined) {
-		data.priority = null;
+		data.priority = "";
 	}
 
 	if (data.done == undefined) {
-		data.done = false;
+		data.done = "false";
 	}
 
 	app.tasks.push(data);
@@ -248,22 +250,60 @@ $(document).ready(function() {
 	// this is terrible code and I should be punished
 	if (path.search('details') !== -1) {
 		details.hash = window.location.hash.substr(1);
+
+		// fill all the data in the form
 		$.each(app.tasks, function(index) {
 			if (app.tasks[index].id == details.hash) {
 				details.data = app.tasks[index];
 
-				// fill all the data in the form
 				$("#id").val(details.data.id);
 				$("#title").val(details.data.title);
-				$("#note").html(details.data.note);
+				$("#note").val(details.data.note);
 				$("#due").val(details.data.due);
 
+				var priority = "";
 				switch(details.data.priority) {
-					case 1: $("#priority-high").attr("checked", "checked").parent("label").addClass("active"); break;
-					case 2: $("#priority-normal").attr("checked", "checked").parent("label").addClass("active"); break;
-					case 3: $("#priority-low").attr("checked", "checked").parent("label").addClass("active"); break;
+					case "1": priority = "high"; break;
+					case "2": priority = "normal"; break;
+					case "3": priority = "low"; break;
 				}
+
+				$("#priority-" + priority).attr("checked", "checked").parent("label").addClass("active");
+
+				var done = null;
+				switch(details.data.done) {
+					case "true": done = "complete"; break;
+					case "false": done = "incomplete"; break;
+				}
+
+				$("#task-" + done).attr("checked", "checked").parent("label").addClass("active");
 			}
+		});
+
+		// @TODO: move to outer scope
+		function getCurrentData() {
+			return {
+				id: $("#id").val(),
+				title: $("#title").val(),
+				note: $("#note").val(),
+				due: $("#due").val(),
+				priority: $("input[name='priority']:checked").val(),
+				done: $("input[name='done']:checked").val()
+			}
+		}
+
+		function updateTask(data) {
+			$.each(app.tasks, function(index) {
+				if (app.tasks[index].id == data.id) {
+					app.tasks[index] = data;
+				}
+			});
+
+			updateStorage();
+		}
+	
+		$("#save").on("click", function() {
+			updateTask(getCurrentData());
 		});
 	}
 });
